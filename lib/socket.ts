328ec61f299371aca181e17e388f76816dcfe181
Josè Pavese <pvsjso@gmail.com>
2025-04-22 10:39:56 +0200

    Update socket.ts

--- lib/socket.ts
+++ lib/socket.ts
@@ -8,165 +8,206 @@ import { version } from "../package.json";
  * possible connection for peers.
  */
 export class Socket extends EventEmitter {
-	private _disconnected: boolean = true;
-	private _id?: string;
-	private _messagesQueue: Array<object> = [];
-	private _socket?: WebSocket;
-	private _wsPingTimer?: any;
-	private readonly _baseUrl: string;
+    private _disconnected: boolean = true;
+    private _id?: string;
+    private _token?: string;
+    private _messagesQueue: Array<object> = [];
+    private _socket?: WebSocket;
+    private _wsPingTimer?: any;
+    private _closed: boolean = false;
+    private readonly _baseUrl: string;
 
-	constructor(
-		secure: any,
-		host: string,
-		port: number,
-		path: string,
-		key: string,
-		private readonly pingInterval: number = 5000,
-	) {
-		super();
+    constructor(
+        secure: any,
+        host: string,
+        port: number,
+        path: string,
+        key: string,
 
-		const wsProtocol = secure ? "wss://" : "ws://";
+        private readonly reconnectDelay = 2000,      // Initial delay (ms)
+        private readonly maxReconnectDelay = 30000,   // Max delay (ms)
+        private reconnectAttempts = 0,     // Number of attempts
+        private readonly pingInterval: number = 5000,
+    ) {
+        super();
 
-		this._baseUrl = wsProtocol + host + ":" + port + path + "peerjs?key=" + key;
-	}
+        const wsProtocol = secure ? "wss://" : "ws://";
 
-	start(id: string, token: string): void {
-		this._id = id;
+        this._baseUrl = wsProtocol + host + ":" + port + path + "peerjs?key=" + key;
+    }
 
-		const wsUrl = `${this._baseUrl}&id=${id}&token=${token}`;
+    start(id: string, token: string): void {
+        this._id = id;
+        this._token = token
 
-		if (!!this._socket || !this._disconnected) {
-			return;
-		}
+        const wsUrl = `${this._baseUrl}&id=${id}&token=${token}`;
 
-		this._socket = new WebSocket(wsUrl + "&version=" + version);
-		this._disconnected = false;
+        if (!!this._closed) {
+            return;
+        };
 
-		this._socket.onmessage = (event) => {
-			let data;
+        if (this._socket) {
+            this._cleanup();
+            this._disconnected = true;
+        };
 
-			try {
-				data = JSON.parse(event.data);
-				logger.log("Server message received:", data);
-			} catch (e) {
-				logger.log("Invalid server message", event.data);
-				return;
-			}
+        this._socket = new WebSocket(wsUrl + "&version=" + version);
+        this._disconnected = false;
 
-			this.emit(SocketEventType.Message, data);
-		};
+        this._socket.onmessage = (event) => {
+            let data;
 
-		this._socket.onclose = (event) => {
-			if (this._disconnected) {
-				return;
-			}
+            try {
+                data = JSON.parse(event.data);
+                logger.log("Server message received:", data);
+            } catch (e) {
+                logger.log("Invalid server message", event.data);
+                return;
+            }
 
-			logger.log("Socket closed.", event);
+            this.emit(SocketEventType.Message, data);
+        };
 
-			this._cleanup();
-			this._disconnected = true;
+        this._socket.onclose = (event) => {
 
-			this.emit(SocketEventType.Disconnected);
-		};
+            if(this._closed) {
+                logger.log(`Socket closed, not trying to reconnect`); 
+                return;
+            }
 
-		// Take care of the queue of connections if necessary and make sure Peer knows
-		// socket is open.
-		this._socket.onopen = () => {
-			if (this._disconnected) {
-				return;
-			}
+            logger.log("Socket closed.", event);
 
-			this._sendQueuedMessages();
+            this._cleanup();
+            this._disconnected = true;
+            this._scheduleReconnect();
 
-			logger.log("Socket open");
+            this.emit(SocketEventType.Disconnected);
+        };
 
-			this._scheduleHeartbeat();
-		};
-	}
+        this._socket.onerror = (event) => {
+            logger.log("Socket error:", event);
+            this._cleanup();
+            this._disconnected = true;
+            
+            this.emit(SocketEventType.Error, event);
+        };
 
-	private _scheduleHeartbeat(): void {
-		this._wsPingTimer = setTimeout(() => {
-			this._sendHeartbeat();
-		}, this.pingInterval);
-	}
+        // Take care of the queue of connections if necessary and make sure Peer knows
+        // socket is open.
+        this._socket.onopen = () => {
+            if (this._disconnected) {
+                return;
+            }
 
-	private _sendHeartbeat(): void {
-		if (!this._wsOpen()) {
-			logger.log(`Cannot send heartbeat, because socket closed`);
-			return;
-		}
+            this._sendQueuedMessages();
 
-		const message = JSON.stringify({ type: ServerMessageType.Heartbeat });
+            logger.log("Socket open");
 
-		this._socket!.send(message);
+            this._scheduleHeartbeat();
+            this.reconnectAttempts = 0;
+        };
+    }
 
-		this._scheduleHeartbeat();
-	}
+    private _scheduleHeartbeat(): void {
+        this._wsPingTimer = setTimeout(() => {
+            this._sendHeartbeat();
+        }, this.pingInterval);
+    }
 
-	/** Is the websocket currently open? */
-	private _wsOpen(): boolean {
-		return !!this._socket && this._socket.readyState === 1;
-	}
+    private _sendHeartbeat(): void {
+        if (!this._wsOpen()) {
+            logger.log(`Cannot send heartbeat, because socket closed`);
+            return;
+        }
 
-	/** Send queued messages. */
-	private _sendQueuedMessages(): void {
-		//Create copy of queue and clear it,
-		//because send method push the message back to queue if smth will go wrong
-		const copiedQueue = [...this._messagesQueue];
-		this._messagesQueue = [];
+        const message = JSON.stringify({ type: ServerMessageType.Heartbeat });
 
-		for (const message of copiedQueue) {
-			this.send(message);
-		}
-	}
+        this._socket!.send(message);
 
-	/** Exposed send for DC & Peer. */
-	send(data: any): void {
-		if (this._disconnected) {
-			return;
-		}
+        this._scheduleHeartbeat();
+    }
 
-		// If we didn't get an ID yet, we can't yet send anything so we should queue
-		// up these messages.
-		if (!this._id) {
-			this._messagesQueue.push(data);
-			return;
-		}
+    _scheduleReconnect() {
+        if(this._closed) {
+            logger.log(`Socket closed, not trying to reconnect`); 
+            return;
+        };
 
-		if (!data.type) {
-			this.emit(SocketEventType.Error, "Invalid message");
-			return;
-		}
+        this.reconnectAttempts++;
+        // max value of reconnect attempts
+        if (this.reconnectAttempts > 10) {
+            this.reconnectAttempts = 10;
+        };
 
-		if (!this._wsOpen()) {
-			return;
-		}
+        const delay = Math.min(this.reconnectDelay * this.reconnectAttempts, this.maxReconnectDelay);
+        console.log(`[Socket] Riconnessione in ${delay} ms...`);
 
-		const message = JSON.stringify(data);
+        // If we are disconnected, we should try to reconnect.
+        setTimeout(() => {
+            this.start(this._id, this._token);
+        }, delay);
+    }
 
-		this._socket!.send(message);
-	}
+    /** Is the websocket currently open? */
+    private _wsOpen(): boolean {
+        return !!this._socket && this._socket.readyState === 1;
+    }
 
-	close(): void {
-		if (this._disconnected) {
-			return;
-		}
+    /** Send queued messages. */
+    private _sendQueuedMessages(): void {
+        //Create copy of queue and clear it,
+        //because send method push the message back to queue if smth will go wrong
+        const copiedQueue = [...this._messagesQueue];
+        this._messagesQueue = [];
 
-		this._cleanup();
+        for (const message of copiedQueue) {
+            this.send(message);
+        }
+    }
 
-		this._disconnected = true;
-	}
+    /** Exposed send for DC & Peer. */
+    send(data: any): void {
+        if (this._disconnected) {
+            return;
+        }
 
-	private _cleanup(): void {
-		if (this._socket) {
-			this._socket.onopen =
-				this._socket.onmessage =
-				this._socket.onclose =
-					null;
-			this._socket.close();
-			this._socket = undefined;
-		}
+        // If we didn't get an ID yet, we can't yet send anything so we should queue
+        // up these messages.
+        if (!this._id) {
+            this._messagesQueue.push(data);
+            return;
+        }
 
-		clearTimeout(this._wsPingTimer!);
-	}
+        if (!data.type) {
+            this.emit(SocketEventType.Error, "Invalid message");
+            return;
+        }
+
+        if (!this._wsOpen()) {
+            return;
+        }
+
+        const message = JSON.stringify(data);
+
+        this._socket!.send(message);
+    }
+
+    close(): void {
+        this._cleanup();
+        this._closed = true;
+        this._disconnected = true;
+    }
+
+    private _cleanup(): void {
+        if (this._socket) {
+            this._socket.onopen =
+                this._socket.onmessage =
+                this._socket.onclose =
+                null;
+            this._socket.close();
+            this._socket = undefined;
+        }
+
+        clearTimeout(this._wsPingTimer!);
+    }
 }
